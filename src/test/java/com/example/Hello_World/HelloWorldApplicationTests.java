6b65ff038839003f8f66f39b9090e1a23ca8638a
ashwiniboddu <144404998+ashwiniboddu@users.noreply.github.com>
2025-07-11 10:24:32 +0530

    Update HelloWorldApplicationTests.java

--- src/test/java/com/example/Hello_World/HelloWorldApplicationTests.java
+++ src/test/java/com/example/Hello_World/HelloWorldApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.Hello_World;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HelloWorldApplication.class)
 class HelloWorldApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
